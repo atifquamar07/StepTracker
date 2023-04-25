644977b2d3b3263fcd600fdc9a6cc8856479d6b6
Mohammad Atif Quamar <atif20523@iiitd.ac.in>
2023-04-25 20:28:51 +0530

    step counter done, button UI improved, direction left

--- app/src/main/java/com/example/steptracker/CountSteps.kt
+++ app/src/main/java/com/example/steptracker/CountSteps.kt
@@ -21,6 +21,9 @@ class CountSteps : AppCompatActivity(), SensorEventListener {
     private var maxDelay: Float = 20000.0f
     private var stepCount = 0
     private var currEWMA = 0.0
+    private var isSensorsActive = false
+    private var isButtonClicked = false
+
 
     private lateinit var tvStepCount: TextView
 
@@ -33,50 +36,59 @@ class CountSteps : AppCompatActivity(), SensorEventListener {
         sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
         accelerometerSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
         Log.i("sampling rate", "$maxDelay")
-
-        btnStopSensor = findViewById(R.id.btn_stop_sensor)
         onPause()
-        var isStarted = false
+        btnStopSensor = findViewById(R.id.btn_stop_sensor)
         btnStopSensor.setOnClickListener {
-            if(!isStarted){
-                isStarted = true
-                btnStopSensor.text = "Stop Walking"
-                stepCount = 0
-                tvStepCount.text = "Steps Count: $stepCount"
-                btnStopSensor.setBackgroundColor(ContextCompat.getColor(this, R.color.red))
-                onResume()
-            }
-            else {
-                isStarted = false
-                stepCount/=10
-                tvStepCount.text = "Steps Count: $stepCount"
-                btnStopSensor.text = "Start Walking"
-                btnStopSensor.setBackgroundColor(ContextCompat.getColor(this, R.color.green))
-                onPause()
-            }
+            toggleSensorListener()
+            isButtonClicked = true
         }
 
     }
 
+    private fun toggleSensorListener() {
+        if (isSensorsActive) {
+            stepCount/=10
+            tvStepCount.text = "Steps Count: $stepCount"
+            btnStopSensor.text = "Start Walking"
+            btnStopSensor.setBackgroundColor(ContextCompat.getColor(this, R.color.green))
+            sensorManager.unregisterListener(this)
+        } else {
+            btnStopSensor.text = "Stop Walking"
+            stepCount = 0
+            tvStepCount.text = "Steps Count: $stepCount"
+            btnStopSensor.setBackgroundColor(ContextCompat.getColor(this, R.color.red))
+            sensorManager.registerListener(this, accelerometerSensor, SensorManager.SENSOR_DELAY_GAME)
+        }
+        isSensorsActive = !isSensorsActive
+    }
+
+
     override fun onResume() {
         super.onResume()
-        sensorManager.registerListener(this, accelerometerSensor, SensorManager.SENSOR_DELAY_GAME)
+        if (isButtonClicked && isSensorsActive) {
+            sensorManager.registerListener(this, accelerometerSensor, SensorManager.SENSOR_DELAY_GAME)
+        }
     }
 
     override fun onPause() {
         super.onPause()
-        sensorManager.unregisterListener(this)
+        if (isButtonClicked) {
+            sensorManager.unregisterListener(this)
+        }
     }
 
+
     override fun onSensorChanged(event: SensorEvent) {
+
         val xAcceleration = event.values[0]
         val yAcceleration = event.values[1]
-        val zAcceleration = event.values[2]
-
+        var zAcceleration = event.values[2]
+        zAcceleration -= 9.81f
+//        Log.i("Acc", "x: $xAcceleration, y: $yAcceleration, z: $zAcceleration")
         val magnitudeAcceleration = sqrt(xAcceleration*xAcceleration + yAcceleration*yAcceleration + zAcceleration*zAcceleration)
         currEWMA = calculateEWMA(magnitudeAcceleration, currEWMA, 0.1f)
         Log.i("Values", "magnitudeAcceleration = $magnitudeAcceleration, currEWMA = $currEWMA")
-        if(magnitudeAcceleration > currEWMA){
+        if(magnitudeAcceleration > currEWMA+1){
             stepCount+=1
         }
     }
